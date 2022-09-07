3880b42f05580211b199c67b2dfa28151786d2f6
prof-amer <amarzuki@wisc.edu>
2022-09-08 05:51:08 +0800

    firebase config

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOnoAicIzMCeUd0alixeV3ruc_NJ-Gicw",
+  authDomain: "todo-346b6.firebaseapp.com",
+  projectId: "todo-346b6",
+  storageBucket: "todo-346b6.appspot.com",
+  messagingSenderId: "1060653151293",
+  appId: "1:1060653151293:web:c84a35136f89216cb42654",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
