3afb03fe4712fc699174c824a59275bf0cf40cd7
prof-amer <amarzuki@wisc.edu>
2022-09-08 05:50:58 +0800

    added error validation

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // initial load
 let tasks = [];
 let inputName = "";
+let onError = false;
+let isLoading = true;
 loadTasks();
 
 new Vue({
@@ -17,42 +19,60 @@ new Vue({
       inputName = "";
     },
     deleteTask: function (task) {
-      removeTask(task.id);
-      this.tasks.splice(this.tasks.indexOf(task), 1);
+      removeTask(task);
     },
   },
 });
 
 function loadTasks() {
-  db.collection("todo-items").orderBy("date", "asc")
+  onError = false;
+  db.collection("todo-items")
+    .orderBy("date", "asc")
     .get()
     .then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
-        tasks.push(
-          {name:doc.data().name, 
-            id: doc.id,
-          date: doc.data().date.toDate()}
-          );
+        tasks.push({
+          name: doc.data().name,
+          id: doc.id,
+          date: doc.data().date.toDate(),
+        });
       });
+      isLoading = false;
+    })
+    .catch(() => {
+      isLoading = false;
+      onError = true;
     });
 }
 
 function updateTasks(inputName) {
+  onError = false;
   let currentTime = new Date();
-  db.collection("todo-items").add({
-    name: inputName,
-    date: currentTime
-  }).then((docRef) => {
-    tasks.push({
+  db.collection("todo-items")
+    .add({
       name: inputName,
       date: currentTime,
-      id: docRef.id
     })
-});
+    .then((docRef) => {
+      tasks.push({
+        name: inputName,
+        date: currentTime,
+        id: docRef.id,
+      });})
+    .catch(() => {
+      onError = true;
+    });
 }
 
-function removeTask(id){
-  db.collection("todo-items").doc(id).delete().then(() => {
-    console.log("Document successfully deleted!");
-})
+function removeTask(task) {
+  onError = false;
+  db.collection("todo-items")
+    .doc(task.id)
+    .delete()
+    .then(() => {
+      tasks.splice(tasks.indexOf(task), 1);
+    })
+    .catch(() => {
+      onError = true;
+    });
 }
