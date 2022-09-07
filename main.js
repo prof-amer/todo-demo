e24e502225d14dae07a5ddb04674db0a858f7ba4
prof-amer <amarzuki@wisc.edu>
2022-09-07 22:35:58 +0800

    added vue

---
+++ main.js
@@ -0,0 +1,26 @@
+new Vue({
+  el: "#tasklist",
+  data: {
+    title: "to do list",
+    tasks: [
+      { name: "Today : Internal Meeting" },
+      { name: "Tomorrow : Read a Book" },
+      { name: "Daily Event Joins" },
+    ],
+  },
+  methods: {
+    newItem: function () {
+      if (!this.tasks.name) {
+        return;
+      }
+      this.tasks.push({
+        name: this.tasks.name,
+        del: "",
+      });
+      this.tasks.name = "";
+    },
+    delItem: function (task) {
+      this.tasks.splice(this.tasks.indexOf(task), 1);
+    },
+  },
+});
